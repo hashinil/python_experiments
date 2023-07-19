30b059b2904154e110885c6d93dda85566f9e1dc
Hashini <hashini.liyanage@gmail.com>
2023-07-19 09:59:26 +0200

    Ex3 - handle wrong input in match case

---
+++ ex3.py
@@ -0,0 +1,18 @@
+todos = []
+while True:
+    user_action = input("add , show or exit: ")
+    user_action = user_action.strip()
+    match user_action:
+        case 'add':
+            user_prompt = "Enter a todo:"
+            todo = input(user_prompt)
+            todos.append(todo)
+        case 'show':
+            for item in todos:
+                print(item)
+        case 'exit':
+            break
+        case whatever:
+            print("Wrong Input")
+
+print('bye')

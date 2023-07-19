4a33ae763c390c3e50d36e775769a950059c89e7
Hashini <hashini.liyanage@gmail.com>
2023-07-19 09:21:23 +0200

    Ex1 - input values

---
+++ ex1.py
@@ -0,0 +1,8 @@
+user_prompt = "Enter a Todo: "
+
+user_input1 = input(user_prompt)
+user_input2 = input(user_prompt)
+user_input3 = input(user_prompt)
+
+todos = [user_input1, user_input2, user_input3, "Hello"]
+print(todos)

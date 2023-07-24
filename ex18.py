a590bd57975d1f715378eda32eb69ed0d0d7ac90
Hashini <hashini.liyanage@gmail.com>
2023-07-24 20:39:41 +0200

    Ex18 - exit button

--- ex18.py
+++ ex18.py
@@ -1,27 +1,31 @@
 import PySimpleGUI as sg
 from convert17 import convert
 
-sg.theme("Black")
 label1 = sg.Text("Enter Feet:  ")
 input1 = sg.Input(key="feet")
 
 label2 = sg.Text("Enter inches: ")
 input2 = sg.Input(key="inch")
 
-convert_btn = sg.Button(size=4, image_source='images/add.png', mouseover_colors="LightBlue2")
+convert_btn = sg.Button("Convert", key="convert")
+exit_btn = sg.Button("Exit", key="exit")
 convert_label = sg.Text(key="output", text_color="green")
 
 window = sg.Window("File Compressor",
                    layout=[[label1, input1],
                            [label2, input2],
-                           [convert_btn, convert_label]])
+                           [convert_btn, exit_btn, convert_label]])
 
 while True:
     event, values = window.read()
     print(event)
     print(values)
-    #feet = float(values["feet"])
-    #inch = float(values["inch"])
-    meter = convert(float(values["feet"]), float(values["inch"]))
-    window["output"].update(value=meter)
+    match event:
+        case 'convert':
+            meter = convert(float(values["feet"]), float(values["inch"]))
+            window["output"].update(value=meter)
+        case 'exit':
+            break
+        case sg.WIN_CLOSED:
+            break
 window.close()
